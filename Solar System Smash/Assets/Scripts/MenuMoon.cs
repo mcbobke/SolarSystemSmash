43e47bcd538653ae964f1e0895bff28d7636229b
SiberianRusky <nrusbarsky@ucmerced.edu>
2015-05-23 11:36:12 -0700

    Menu Screen
    Added A Menu Screen With appropriate scripts for the sun and the moon.

---
+++ Solar System Smash/Assets/Scripts/MenuMoon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMoon : MonoBehaviour {
+
+    //edit these values for maximum floating positions, speed and interpolation
+    float maxupanddown = -1;
+    float speed = 100;
+    float angle = 0;
+    float todegrees = Mathf.PI / 180;
+    public Vector3 localPosition;
+
+    void Start()
+    {
+        localPosition = GetComponent<Transform>().position;
+    }
+
+    void Update()
+    {
+        angle += speed * Time.deltaTime;
+
+
+        if (angle > 360) angle -= 360;
+        localPosition.y = maxupanddown * Mathf.Sin(angle * todegrees);
+        GetComponent<Transform>().position = localPosition;
+    }
+}
---
+++ Solar System Smash/Assets/Scripts/MenuSun.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSun : MonoBehaviour {
+    //edit these values to put in 
+    float maxupanddown = 1;
+    float speed = 100;
+    float angle = 0;
+    float todegrees = Mathf.PI/180;
+    public Vector3 localPosition;
+
+    void Start ()
+    {
+        localPosition = GetComponent<Transform>().position;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        angle += speed * Time.deltaTime;
+
+
+        if (angle > 360) angle -= 360;
+        localPosition.y = maxupanddown * Mathf.Sin(angle * todegrees);
+        GetComponent<Transform>().position = localPosition;
+	}
+
+}
