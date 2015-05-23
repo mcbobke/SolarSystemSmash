4be8af3faea28d469372b828e1c72484407947fc
Matthew Bobke <mcbobke@gmail.com>
2015-05-23 13:50:10 -0700

    Fixed weird particles

---
+++ Solar System Smash/Assets/Scripts/Boundary.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boundary : MonoBehaviour
+{
+    private void OnCollsionEnter2D(Collision2D coll)
+    {
+        Debug.Log(coll.gameObject.tag);
+        if (coll.gameObject.tag == "Projectile")
+            Destroy(coll.gameObject);
+    }
+}
