b13622e24428182357bb469c952abe8de3aaa3cf
Matthew Bobke <mcbobke@gmail.com>
2015-05-23 14:42:08 -0700

    Removed an old scene

---
+++ Solar System Smash/Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
