709a969bf453bbefb2fc7c6dfd695756579538b0
SiberianRusky <nrusbarsky@ucmerced.edu>
2015-05-23 14:18:22 -0700

    Added Final Art and Integrated it with Main menu

---
+++ Solar System Smash/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitButton : MonoBehaviour {
+	public Renderer rend;
+	
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	
+	void OnMouseEnter()
+	{
+		rend.material.color = Color.red;
+	}
+	
+	void OnMouseExit() {
+		rend.material.color = Color.white;
+	}
+	
+	void OnMouseDown() {
+		Application.Quit();
+	}
+}
---
+++ Solar System Smash/Assets/Scripts/StartButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton: MonoBehaviour {
+	public Renderer rend;
+    public GameObject myVisible;
+
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+
+	void OnMouseEnter()
+	{
+        myVisible.SetActive(true); 
+	}
+
+	void OnMouseExit() {
+        myVisible.SetActive(false);
+	}
+
+	//void OnMouseDown() {
+	//	Application.LoadLevel("Volcano Game");
+	//}
+
+}
