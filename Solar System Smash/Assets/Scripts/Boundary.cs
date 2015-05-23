a5f4f10b0c90039e582d2d99aafb566397f52563
Matthew Bobke <mcbobke@gmail.com>
2015-05-23 14:17:34 -0700

    Removed unneeded console prints

--- Solar System Smash/Assets/Scripts/Boundary.cs
+++ Solar System Smash/Assets/Scripts/Boundary.cs
@@ -5,8 +5,6 @@ public class Boundary : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D coll)
     {
-        Debug.Log(coll.gameObject.tag + coll.gameObject.name);
-        Debug.Log("this is working");
         if (coll.gameObject.tag == "Projectile")
             Destroy(coll.gameObject);
     }
