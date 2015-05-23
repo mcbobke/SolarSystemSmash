1acf37c0d3073987ceb92329427eea32c612c552
Matthew Bobke <mcbobke@gmail.com>
2015-05-22 23:22:05 -0700

    wa

--- Solar System Smash/Assets/Scripts/SunPlayer.cs
+++ Solar System Smash/Assets/Scripts/SunPlayer.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class SunPlayer : MonoBehaviour
 {
     public Rigidbody2D rb;
+<<<<<<< HEAD
     private Vector2 movement = new Vector2(0f, 10f);
+=======
+    private Vector2 movement = new Vector2(0f, 5f);
+>>>>>>> parent of 134c356... Colliders, colliders, colliders
 
     // Use this for initialization
     private void Start()
