63cf110f5ff947f55e131a74bee94195f776e913
Matthew Bobke <mcbobke@gmail.com>
2015-05-23 11:55:38 -0700

    Rotation for sun to follow mouse cursor

--- Solar System Smash/Assets/Scripts/SunPlayer.cs
+++ Solar System Smash/Assets/Scripts/SunPlayer.cs
@@ -6,21 +6,52 @@ public class SunPlayer : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movement;
     private bool inputFlipped;
+    private Vector3 mousePosition;
 
-    // Use this for initialization
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         movement = new Vector2(0f, 5f);
         inputFlipped = false;
+        mousePosition = Input.mousePosition;
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-            rb.AddForce(movement);
-        else if (Input.GetKey(KeyCode.DownArrow))
-            rb.AddForce(-movement);
+        // Input for movement
+        if (!inputFlipped)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+                rb.AddForce(movement);
+            else if (Input.GetKey(KeyCode.DownArrow))
+                rb.AddForce(-movement);
+        }
+
+        else
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+                rb.AddForce(-movement);
+            else if (Input.GetKey(KeyCode.DownArrow))
+                rb.AddForce(movement);
+        }
+
+        // Getting mouse position and then making sure that the sun faces the mouse with it's "up" vector
+        mousePosition = Input.mousePosition;
+
+        Vector3 diff = Camera.main.ScreenToWorldPoint(mousePosition) - transform.position;
+        diff.Normalize();
+
+        float rotZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ - 90);
+
+        // Fire
+        if (Input.GetMouseButtonDown(0))
+            Fire();
+    }
+
+    private void Fire()
+    {
+        
+
     }
 }
