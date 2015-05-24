3e10199ebdab230e1ed7e254a41522bf1db7e4cd
Matthew Bobke <mcbobke@gmail.com>
2015-05-23 18:56:12 -0700

    Updated planet1 to shoot at players

--- Solar System Smash/Assets/Scripts/Projectile.cs
+++ Solar System Smash/Assets/Scripts/Projectile.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour
 {
+
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Comet")
         {
             Destroy(coll.gameObject);
+            Destroy(gameObject);
+        }
+
+        if (coll.gameObject.tag == "Boundary")
+        {
             Destroy(gameObject);
         }
     }
